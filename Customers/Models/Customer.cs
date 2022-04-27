5b1485a1b427a94b072386d9ecd01812f42d4852
mohamed elfeky <mohamedelfeky700@outlook.com>
2022-04-27 15:33:18 +0200

    Add project files.

---
+++ Customers/Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Customers.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(120)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(120)]
+        public string LastName { get; set; }
+
+        [Required, MaxLength(20)]
+        public string Contact { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
